f31cd2eb356bb5d7f18e8fdbd336e92eeddaecad
Hazel Levine <rose.hazel@protonmail.ch>
2019-09-06 15:30:21 -0400

    minor formatting fixes

--- src/commands/division.js
+++ src/commands/division.js
@@ -11,7 +11,8 @@ module.exports = {
             const separatorRole = msg.guild.roles.get(config.separatorRole);
             let r = 1; // 1 to exclude @everyone
             let roles = [];
-            while (r < separatorRole.position) { // get all roles under seperator role
+            while (r < separatorRole.position) {
+                // get all roles under seperator role
                 const append = msg.guild.roles.find(x => x.position === r);
                 if (!append) error("failed to get usable divisions", r);
                 roles.push(append.id); // store ids for comparison purposes
@@ -25,14 +26,15 @@ module.exports = {
                 debug("position", role.position);
 
                 if (!role) return "Try signing up for a division that exists.";
-                if (roles.map(x => msg.member.roles.has(x)).includes(true)) return "You're already in a division!";
+                if (roles.map(x => msg.member.roles.has(x)).includes(true))
+                    return "You're already in a division!";
                 if (msg.member.roles.has(role.id)) return "You already were :)";
                 if (role.comparePositionTo(separatorRole) >= 0) return "no";
                 await msg.member.addRole(role);
                 return "Added!";
             } else {
-                const current = msg.member.roles.find(
-                    x => roles.includes(x.id)
+                const current = msg.member.roles.find(x =>
+                    roles.includes(x.id)
                 );
                 if (!current) return "Get in a division first!";
                 await msg.member.removeRole(current);
