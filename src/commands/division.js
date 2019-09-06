c896b0305128ce081af0f974ce1974a14f889373
Hazel Levine <rose.hazel@protonmail.ch>
2019-09-06 15:03:17 -0400

    make sure a user can't be in more than 1 division, redo undivision

--- src/commands/division.js
+++ src/commands/division.js
@@ -9,23 +9,34 @@ module.exports = {
                 "valid separator role"
             );
             const separatorRole = msg.guild.roles.get(config.separatorRole);
-            const role = msg.guild.roles.find(
-                x => x.name.toLowerCase() === rawArgs.toLowerCase()
-            );
-            if (!role) return "That division doesn't exist :/";
-            debug("has", msg.member.roles.has(role.id));
+            let r = 1; // 1 to exclude @everyone
+            let roles = [];
+            while (r < separatorRole.position) { // get all roles under seperator role
+                const append = msg.guild.roles.find(x => x.position === r);
+                if (!append) error("failed to get usable divisions", r);
+                roles.push(append.id); // store ids for comparison purposes
+                r++;
+            }
             if (add) {
+                const role = msg.guild.roles.find(
+                    x => x.name.toLowerCase() === rawArgs.toLowerCase()
+                );
+                debug("has", msg.member.roles.has(role.id));
+                debug("position", role.position);
+
+                if (!role) return "Try signing up for a division that exists.";
+                if (roles.map(x => msg.member.roles.has(x)).includes(true)) return "You're already in a division!";
                 if (msg.member.roles.has(role.id)) return "You already were :)";
                 if (role.comparePositionTo(separatorRole) >= 0) return "no";
                 await msg.member.addRole(role);
                 return "Added!";
             } else {
-                if (!msg.member.roles.has(role.id))
-                    return "You weren't but now you aren't even more.";
-                if (role.comparePositionTo(separatorRole) >= 0)
-                    return "yes you are";
-                await msg.member.removeRole(role);
-                return "Removed!";
+                const current = msg.member.roles.find(
+                    x => roles.includes(x.id)
+                );
+                if (!current) return "Get in a division first!";
+                await msg.member.removeRole(current);
+                return "Removed! Get yourself a new division.";
             }
         };
         return {
