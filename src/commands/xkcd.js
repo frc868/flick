a351acbf87efca0154f04530f94761161f64d270
Hazel Levine <rose.hazel@protonmail.ch>
2019-09-07 19:16:45 -0400

    add xkcd functionality (no alttext)

---
+++ src/commands/xkcd.js
@@ -0,0 +1,36 @@
+const { debug, info, error, fatal, assert } = require("../logging.js");
+const discord = require("discord.js");
+const xclient = require("xkcd-api"); // i hate this api, but i don't feel like writing my own crawler
+
+const sendAttachment = (msg, url) => {
+    msg.channel.send("", {
+        files: [
+            new discord.Attachment(url)
+        ]
+    });
+};
+
+const xkcd = () => {
+    return ({ msg, args }) => {
+        if (args.length === 0) {
+            xclient.latest((err, resp) => {
+                if (err) {
+                    error("failed to find random xkcd"); // wtf
+                } else {
+                    sendAttachment(msg, resp.img);
+                }
+            });
+        } else if (args.length === 1) {
+            xclient.get(args[0], (err, resp) => {
+                if (err) {
+                    info("failed to find xkcd", args[0]);
+                    msg.channel.send("Couldn't find that one...");
+                } else {
+                    sendAttachment(msg, resp.img);
+                }
+            });
+        }
+    };
+};
+
+module.exports = { init: () => ({ xkcd: xkcd() }) };
