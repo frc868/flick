8c07fb5c2092dcea607c31cc628aaaf25d7450ac
Hazel Levine <rose.hazel@protonmail.ch>
2019-09-08 00:25:58 -0400

    add important video

--- src/commands/songs.js
+++ src/commands/songs.js
@@ -3,6 +3,7 @@ module.exports = {
         evil: () => "https://youtu.be/dkpgz3uQ58U",
         okgo: () => "https://youtu.be/qybUFnY7Y8w",
         dangerous: () => "https://youtu.be/LpQArtCeXTk",
-        birchtree: () => "https://youtu.be/pFkGvVFdx-o"
+        birchtree: () => "https://youtu.be/pFkGvVFdx-o",
+        poofs: () => "https://www.youtube.com/watch?v=EtaR150-bdkf"
     })
 };
