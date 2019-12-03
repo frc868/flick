88d981c71f7383ab2eca883295fe4b66551e8e19
Hazel Levine <rose.hazel@protonmail.ch>
2019-12-03 10:49:20 -0500

    add random function to ;xkcd

--- src/commands/xkcd.js
+++ src/commands/xkcd.js
@@ -4,6 +4,7 @@ const util = require("util");
 
 const xlatest = util.promisify(xclient.latest);
 const xget = util.promisify(xclient.get);
+const xrandom = util.promisify(xclient.random);
 
 module.exports = {
     commands: { xkcd: null },
@@ -19,9 +20,15 @@ module.exports = {
                     fields: [{ title: "Alt text", value: ret.alt }]
                 };
             } else if (args.length === 1) {
-                const ret = await xget(args[0]).catch(_ =>
-                    info("failed to get xkcd", args[0])
-                );
+                let ret;
+                if (args[0].localeCompare("random") === 0)
+                    ret = await xrandom().catch(_ =>
+                        info("failed to get random xkcd")
+                    );
+                else
+                    ret = await xget(args[0]).catch(_ =>
+                        info("failed to get xkcd", args[0])
+                    );
                 if (ret)
                     return {
                         title: "XKCD " + ret.num + ": " + ret.title,
